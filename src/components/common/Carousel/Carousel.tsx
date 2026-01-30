874a0821fd20211595d3194458e6c28158b75607
Pradeeshkumar-k <pradeeshkumark600@gmail.com>
2026-01-30 13:24:03 +0530

    fix

--- src/components/common/Carousel/Carousel.tsx
+++ src/components/common/Carousel/Carousel.tsx
@@ -59,6 +59,7 @@ const Carousel: React.FC = () => {
                     <img
                       src={author.image}
                       alt={author.name}
+                      loading="lazy"
                       className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
                     />
                   </div>
